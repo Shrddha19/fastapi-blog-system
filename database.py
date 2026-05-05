792bf73c5331b94bdbb8e247fdb21ce2275a3735
Shrddha19 <nikamshrdha19@gmail.com>
2026-05-05 23:17:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-database_url="mysql+pymysql://root:1234567@localhost/fastapi"
+database_url="mysql+pymysql://root:yourpassword@localhost/fastapi"
 engine=create_engine(database_url)
 sessionlocal=sessionmaker(bind=engine)
 base=declarative_base()
